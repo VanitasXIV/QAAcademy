1c044f91ba22327bfca835eea2f300ec25233ec7
Ivan <ivangonzalogomez@gmail.com>
2025-06-19 10:27:10 -0300

    Added EquipoAsignadoTest

--- src/test/java/org/example/EquipoAsignadoTest.java
+++ src/test/java/org/example/EquipoAsignadoTest.java
@@ -8,9 +8,10 @@ import org.junit.jupiter.api.Test;
 public class EquipoAsignadoTest extends TestFunctions{
     String homePage = "http://127.0.0.1:5500/QAAcademy/src/main/resources/TorneoHTML/HomePage.html";
     String equiposAsignadosUrl = "http://127.0.0.1:5500/QAAcademy/src/main/resources/TorneoHTML/equipos.html";
-
+    String cardContainerXpath="";
     //Xpath
     String equiposAsignadosButtonXpath = "/html/body/nav/div/ul[1]/li[3]/a";
+    int cantidadEquipos = 8;
 
     @BeforeEach
     public void setUp() {
@@ -26,6 +27,8 @@ public class EquipoAsignadoTest extends TestFunctions{
 
     @Test
     public void equipoCardsContainElements(){
-        //TODO
+        for(int i = 0; i<cantidadEquipos; i++){
+
+        }
     }
 }
